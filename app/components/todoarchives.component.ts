f62bd0fb75d3f271a23d7f7a40ee87ee0a663431
Basile Beldame <basile.beldame@supdeweb-paris.com>
2016-10-13 21:54:58 +0200

    US6

--- app/components/todoarchives.component.ts
+++ app/components/todoarchives.component.ts
@@ -1,17 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TodoService } from '../services/todo.service';
+import { TodoItem } from '../data.model';
 
 @Component({
   selector: 'homepage-component',
   template: `
     <section>
-      <article>
-      Page archives
+      <article *ngFor="let todo of todos">
+        <!--On verifie ici que le todo est fait-->
+        <div *ngIf="todo.done">
+          <p><u>Todo numéro {{todo.id}} :</u></p>
+          <p><b>{{todo.title}}</b></p>
+          <p>{{todo.details}}</p>
+          <hr>
+        </div>
       </article>
     </section>
-  `
+  `,
+  providers: [ TodoService ]
 })
 
 export class ToDoArchivesComponent {
+    todos: TodoItem[];
+
+    constructor(
+        private todoService: TodoService
+    ){};
+
+    getTodos() {
+        this.todoService.getTodos()
+            .then(a => this.todos = a);
+    }
+
+    ngOnInit() {
+        this.getTodos();
+    }
 }
